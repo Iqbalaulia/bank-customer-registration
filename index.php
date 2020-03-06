36ec19dd55702e2f57eb7103eae8ba1bcb8d51b9
Iqbalaulia <16410100012@stikom.edu>
2020-03-06 22:56:02 +0700

    change ui login

--- index.php
+++ index.php
@@ -18,76 +18,67 @@
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <link rel="stylesheet" href="dist/css/custome.css">
+
 </head>
 
-<body class="hold-transition login-page">
-  <div class="login-box">
+<body class="hold-transition login-page bg-white">
 
-    <!-- /.login-logo -->
-    <div class="card">
-      <div class="card-body login-card-body">
-        <p class="login-box-msg">Selamat datang !</p>
 
-        <form action="./function/validation-login.php" method="post">
-          <div class="input-group mb-3">
-            <input type="text" name="nip" class="form-control" placeholder="NIP">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
+  <section class="head mt-5">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col align-self-center">
+          <div class="login-box mr-5">
+            <div class="card">
+              <div class="card-body login-card-body">
+                <p class="login-box-msg">Selamat datang !</p>
+
+                <form action="./function/validation-login.php" method="post">
+                  <div class="input-group mb-3">
+                    <input type="text" name="nip" class="form-control" placeholder="NIP">
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="input-group mb-3">
+                    <input type="password" name="password" class="form-control" placeholder="Password">
+                    <div class="input-group-append">
+                      <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-8">
+                      <div class="icheck-primary">
+                        <input type="checkbox" id="remember">
+                        <label for="remember">
+                          Remember Me
+                        </label>
+                      </div>
+                    </div>
+                    <div class="col-4">
+                      <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+                  </div>
+                </form>
+                
+                    <a href="register-user.php" class="btn-register text-center btn btn-outline-primary mt-2">Register a new teller</a>
+                
               </div>
             </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control" placeholder="Password">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-8">
-              <div class="icheck-primary">
-                <input type="checkbox" id="remember">
-                <label for="remember">
-                  Remember Me
-                </label>
-              </div>
-            </div>
-            <!-- /.col -->
-            <div class="col-4">
-              <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-            </div>
-            <!-- /.col -->
-          </div>
-        </form>
-
-        <!-- <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div> -->
-        <!-- /.social-auth-links -->
-
-        <!-- <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p> -->
-
-        <div class="row">
-          <div class="col-md-12">
-            <p class="mb-0">
-              <a href="register-user.php" class="text-center btn btn-outline-primary">Register a new teller</a>
-            </p>
           </div>
         </div>
+        <div class="col d-none d-sm-block d-sm-none d-md-block">
+          <img width="600" src="dist/img/hero-icon.png" alt="">
+        </div>
       </div>
-      <!-- /.login-card-body -->
     </div>
-  </div>
+  </section>
   <!-- /.login-box -->
 
   <!-- jQuery -->
